b6c77d7ebee1156db4c944f836afc192a10e7cfe
irener07 <43020903+irener07@users.noreply.github.com>
2018-09-06 20:55:48 -0600

    Update Book.java

--- book/Book.java
+++ book/Book.java
@@ -1,5 +1,12 @@
 package book;
 
 public class Book {
+ 
+    //Atributos de la clase
+    private int Issn, CantidadVendida, CantidadDisponible, precio;
+    private String nombre, tema, descripcion;
+    private image 
+    
+
     
 }
