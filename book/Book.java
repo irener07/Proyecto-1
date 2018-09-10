c46e36f5c2562f826103d62b21ddef8aaee6378d
Vale-Martinez <42896468+Vale-Martinez@users.noreply.github.com>
2018-09-09 18:58:58 -0600

    Update Book.java

--- book/Book.java
+++ book/Book.java
@@ -5,6 +5,5 @@ public class Book {
     //Atributos de la clase
     private int Issn, CantidadVendida, CantidadDisponible, precio;
     private String nombre, tema, descripcion;
-    private image 
     
 }
